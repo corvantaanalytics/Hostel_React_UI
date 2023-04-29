4596fa8ac27d307e0928e3e0e7ef70bd0d1f446a
mdsameer2k <mdsameer2k@gmail.com>
2023-04-29 17:11:00 +0530

    Removed Unwanted code

--- src/pages/Dashboard/Home/Home.page.jsx
+++ src/pages/Dashboard/Home/Home.page.jsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { DashboardLayout } from "layout";
 import Text from "components/Text/Text.component";
 import Section from "components/Section/Section.component";
+
+
 const Home = () => {
   
-
   return (
     <DashboardLayout>
       <div className="bg-[#08090A] gap-5 p-10 text-white h-full">
