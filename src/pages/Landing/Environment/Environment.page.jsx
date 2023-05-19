d4ca90b6a489ad4e9b8d109d13a0e2aed2c1ef3d
Sanofer Sameena <samee2914@gmail.com>
2023-05-19 20:09:17 +0530

    Landing page Folder Created for section

---
+++ src/pages/Landing/Environment/Environment.page.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { Row, Col, Card } from "antd";
+import { Container } from "react-bootstrap";
+import Carousel from "better-react-carousel";
+import "./Environment.styles.scss";
+
+const Environment = () => {
+  return (
+    <>
+      <Container>
+        <div className="p-3">
+          <p className="title-card" style={{ color: "#008080" }}>
+            The Environment at WoW
+          </p>
+          <p className="subhead" style={{ color: "#596e79" }}>
+            we built cultural spaces into your environment.
+          </p>
+        </div>
+      </Container>
+      <Carousel cols={3} rows={1} gap={10} loop>
+        <Carousel.Item>
+          <img
+            src="/img/assets/environment/Swimming_pool.jpg"
+            className="img-border"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            src="/img/assets/environment/building.jpg"
+            className="img-border"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src="/img/assets/environment/room.jpg" className="img-border" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            src="/img/assets/environment/openarea.jpg"
+            className="img-border"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            src="/img/assets/environment/Dinnerarea.jpg"
+            className="img-border"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            src="/img/assets/environment/entertainment.jpg"
+            className="img-border"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src="/img/assets/environment/gym.jpg" className="img-border" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            src="/img/assets/environment/playarea.jpg"
+            className="img-border"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            src="/img/assets/environment/kitchen.jpg"
+            className="img-border"
+          />
+        </Carousel.Item>
+      </Carousel>
+    </>
+  );
+};
+export default Environment;
---
+++ src/pages/Landing/Location/Location.page.jsx
@@ -0,0 +1,114 @@
+import React from "react";
+import { Row, Col, Card } from "antd";
+import { Container, Button } from "react-bootstrap";
+import { useNavigate } from "react-router";
+import "./Location.styles.scss";
+
+const Location = () => {
+  const navigate = useNavigate();
+  const navigateToSignin = () => {
+    navigate("/landing/sign-in");
+  };
+  return (
+    <>
+      <Container className="px-5">
+        <p className="head center pb-5">Location</p>
+        <Row>
+          <Col lg={8}>
+            <Row>
+              <Col>
+                <p className="pr-3 subhead">@</p>
+              </Col>
+              <Col>
+                <p className="subhead">Chrompet</p>
+                <ul
+                  className="workplace-text pb-3"
+                  style={{ color: "#F4C2C2" }}
+                >
+                  <li>52, east street</li>
+                  <li>chrompet</li>
+                  <li>Chennai</li>
+                  <li>
+                    <Button
+                      type="ghost"
+                      className="learnButton px-4 py-2 mt-3 hover:#F08EDB text-white rounded-md hover:bg-pink-600/[.8] ease-in duration-200"
+                      style={{ border: "none" }}
+                      onClick={navigateToSignin}
+                    >
+                      Book Now
+                    </Button>
+                  </li>
+                </ul>
+              </Col>
+            </Row>
+          </Col>
+          <Col lg={8}>
+            <Row>
+              <Col>
+                <p className="pr-3 subhead">@</p>
+              </Col>
+              <Col>
+                <p className="subhead">Chrompet</p>
+                <ul
+                  className="workplace-text pb-3"
+                  style={{ color: "#F4C2C2" }}
+                >
+                  <li>52, east street</li>
+                  <li>chrompet</li>
+                  <li>Chennai</li>
+                  <li>
+                    <Button
+                      type="ghost"
+                      className="learnButton px-4 py-2 mt-3 hover:#F08EDB text-white rounded-md hover:bg-pink-600/[.8] ease-in duration-200"
+                      style={{ border: "none" }}
+                      onClick={navigateToSignin}
+                    >
+                      Book Now
+                    </Button>
+                  </li>
+                </ul>
+              </Col>
+            </Row>
+          </Col>
+          <Col lg={8}>
+            <Row>
+              <Col>
+                <p className="pr-3 subhead">@</p>
+              </Col>
+              <Col>
+                <p className="subhead">Chrompet</p>
+                <ul
+                  className="workplace-text pb-3"
+                  style={{ color: "#F4C2C2" }}
+                >
+                  <li>52, east street</li>
+                  <li>chrompet</li>
+                  <li>Chennai</li>
+                  <li>
+                    <Button
+                      type="ghost"
+                      className="learnButton px-4 py-2 mt-3 hover:#F08EDB text-white rounded-md hover:bg-pink-600/[.8] ease-in duration-200"
+                      style={{ border: "none" }}
+                      onClick={navigateToSignin}
+                    >
+                      Book Now
+                    </Button>
+                  </li>
+                </ul>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+        <div className="center pt-5">
+          <p className="workplace-time" style={{ color: "#FFFFFF" }}>
+            Call us on{" "}
+            <span className="workplace-time" style={{ color: "#EEEA89" }}>
+              +91-847-4825422
+            </span>
+          </p>
+        </div>
+      </Container>
+    </>
+  );
+};
+export default Location;
